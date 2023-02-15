3424142c69b846579b93c380c41c1113cc695133
UchidaMizuki <uchidamizuki@vivaldi.net>
2023-02-16 00:59:02 +0900

    Remove memoise

--- R/zzz.R
+++ R/zzz.R
@@ -1,7 +1,7 @@
 .onLoad <- function(...) {
-  read_mojxml_crs <<- memoise::memoise(read_mojxml_crs)
-  read_mojxml_attr <<- memoise::memoise(read_mojxml_attr)
-  read_mojxml_geometry_point <<- memoise::memoise(read_mojxml_geometry_point)
-  read_mojxml_geometry_line <<- memoise::memoise(read_mojxml_geometry_line)
-  read_mojxml_geometry_polygon <<- memoise::memoise(read_mojxml_geometry_polygon)
+  # read_mojxml_crs <<- memoise::memoise(read_mojxml_crs)
+  # read_mojxml_attr <<- memoise::memoise(read_mojxml_attr)
+  # read_mojxml_geometry_point <<- memoise::memoise(read_mojxml_geometry_point)
+  # read_mojxml_geometry_line <<- memoise::memoise(read_mojxml_geometry_line)
+  # read_mojxml_geometry_polygon <<- memoise::memoise(read_mojxml_geometry_polygon)
 }
